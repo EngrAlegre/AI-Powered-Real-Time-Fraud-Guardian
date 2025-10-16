21f98bf96c91bf2fad9b950001014bb48f1be55c
Engr_Alegre <qjisalegre@tip.edu.ph>
2025-10-16 17:25:40 +0800

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6663759873-2a29d",
-  "appId": "1:869248609203:web:4cd475ea2726a21958b9c6",
-  "apiKey": "AIzaSyBExOBgYIzerkKdaCBEoSOlhqlddfQVWT0",
-  "authDomain": "studio-6663759873-2a29d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "869248609203"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
 };
